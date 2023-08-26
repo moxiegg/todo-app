9b3b783d6a40df574f2196bbbdb10e4a1ffd2207
moxiegg <aravindh19.7k@gmail.com>
2023-08-26 14:23:35 +0530

    Updated the sidebar UI

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,14 +1,23 @@
 import { updateContent } from "./uiModule.js";
 import {project,showProject} from "./projectModule.js";
+import "./style/mainStyle.css";
 export function createSidebarElement(projList){
     const sidebarElement=document.createElement("div");
+    const projListELement=document.createElement("div");
+    projListELement.className="projListElement";
     projList.forEach(projObj => {
         let extraDiv=document.createElement("div");
+        extraDiv.className="projectCard";
         extraDiv.textContent=projObj.getName();
         extraDiv.addEventListener('click',()=>{
             updateContent(showProject(projObj));
-        })
-        sidebarElement.append(extraDiv);
+        });
+        const delBtn = document.createElement("button");
+        delBtn.textContent="Delete";
+        delBtn.className="card-button";
+        extraDiv.append(delBtn);
+        projListELement.append(extraDiv);
     });
+    sidebarElement.append(projListELement);
     return sidebarElement;
 }
